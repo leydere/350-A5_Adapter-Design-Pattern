845ee450cfc90d5f2c615a467e333cebd600accd
Eric Leyder <leydere@ferris.edu>
2020-03-11 16:55:58 -0400

    Initial commit

---
+++ src/ICarSpec.java
@@ -0,0 +1,8 @@
+
+public interface ICarSpec {
+	public int getMaxSpeed();
+
+	public String getfullCarname();
+
+	public double getCarFuelCap();
+}
---
+++ src/RacingCar.java
@@ -0,0 +1,34 @@
+
+public class RacingCar {
+	
+	public int getTopSpeedRacingMode() {
+		
+		return -1;
+	}
+
+	public int getTopSpeedNormalMode() {
+		
+		return -1;
+	}
+
+	public String getMake() {
+		
+		return "ham";
+	}
+
+	public String getModel() {
+		
+		return "ham";
+	}
+
+	public String getYear() {
+		
+		return "ham";
+	}
+
+	//set this to return double; assignment did not declare it as return type or void so made assumption
+	public double getCarFuelCapacity() {
+		
+		return -1;
+	}
+}
